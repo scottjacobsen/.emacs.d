a201df686c1247bff65ad32e833319f9016bd9d4
Scott Jacobsen <jacobsenscott@yahoo.com>
2013-12-19 10:07:53 -0700

    Use robe in haml

--- custom/02haml.el
+++ custom/02haml.el
@@ -1,5 +1,6 @@
 (setq auto-mode-alist (cons '("\\.haml" . haml-mode) auto-mode-alist))
 (add-hook 'haml-mode (lambda() (setq tab-width 4)))
+(add-hook 'haml-mode 'robe-mode)
 (add-hook 'haml-mode-hook
           (lambda ()
             (set (make-local-variable 'electric-indent-functions)
