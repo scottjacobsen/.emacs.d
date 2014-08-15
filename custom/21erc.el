6df65bb7567a1c7021e320624efbea6ad2b8ef0d
Scott Jacobsen <jacobsenscott@yahoo.com>
2014-08-15 10:29:35 -0600

    Make ERC work.
    Add some chat channels
    
    Add a shortcut to connect to bitlbee

--- custom/21erc.el
+++ custom/21erc.el
@@ -3,6 +3,8 @@
 ;;; Customizations for ERC
 ;;;
 ;;; Code:
+(require 'erc)
+
 (defun my-reformat-jabber-backlog ()
   "Fix \"unkown participant\" backlog messages from bitlbee."
   (save-excursion
@@ -18,10 +20,34 @@
 (add-hook 'erc-join-hook 'bitlbee-identify)
 (defun bitlbee-identify ()
   "If we're on the bitlbee server, send the identify command to the &bitlbee channel."
-  (when (and (string= "localhost" erc-session-server)
+  (when (and (string= "bitlbee" erc-session-server)
              (string= "&bitlbee" (buffer-name)))
     (erc-message "PRIVMSG" (format "%s identify %s"
                                    (erc-default-target)
                                    bitlbee-password))))
+
+(defun chat-connect ()
+  "Connects chat sessions."
+  (erc :server "bitlbee"
+       :nick "scott"
+       :password (concat "scott/bitlbee:" bitlbee-password))
+  (erc :server "freenode"
+       :nick "scott"
+       :password (concat "scott/freenode:" bitlbee-password))
+  (erc :server "oftc"
+       :nick "scott"
+       :password (concat "scott/oftc:" bitlbee-password))
+  (erc :server "mozilla"
+       :nick "mozilla"
+       :password (concat "scott/mozilla:" bitlbee-password)))
+
+(global-set-key (kbd "C-c eb") (lambda() (interactive) (chat-connect)))
+
+(setq erc-autojoin-channels-alist '(("freenode" "#emacs")
+                                    ("bitlbee" "#neighborland" "#twitter_jacobsenscott")
+                                    ("oftc" "#bitlbee")))
+
+
+
 (provide '21erc)
 ;;; 21erc ends here
