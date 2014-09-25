fdff78c13b70062faf9ef08c22f9a984f3dcca20
Scott Jacobsen <jacobsenscott@yahoo.com>
2014-09-25 14:23:25 -0600

    Update ruby version

--- custom/20chruby.el
+++ custom/20chruby.el
@@ -1,3 +1,8 @@
+;;; chruby --- Settings for chruby
+;;; Commentary:
+;;; Settings yo.
+;;; Code:
 (require 'chruby)
-(chruby "ruby-2.1.2")
+(chruby "ruby-2.1.3")
 (provide '20chruby)
+;;; 20chruby.el ends here
