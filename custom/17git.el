ddc12e11936c3ded0aa3aa57b621cf797d5209d8
Scott Jacobsen <jacobsenscott@yahoo.com>
2015-01-15 23:09:53 -0700

    Move git-root to custom/*git* file

--- custom/17git.el
+++ custom/17git.el
@@ -1,2 +1,25 @@
+;;; 17git -- git stuff
+;;; Commentary:
+;;; git stuff
+;;; Code:
+(defun git-root ()
+  "Return GIT_ROOT if this file is a part of a git rep,
+else return nil"
+  (let ((curdir default-directory)
+        (max 10)
+        (found nil))
+    (while (and (not found) (> max 0))
+      (progn
+        (if (file-directory-p (concat curdir ".git"))
+            (progn
+              (setq found t))
+          (progn
+            (setq curdir (concat curdir "../"))
+            (setq max (- max 1))))))
+    (if found (expand-file-name curdir))))
+
 (global-set-key (kbd "C-x v s") 'magit-status)
+
 (provide '17git)
+
+;;; 17git.el ends here
