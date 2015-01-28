c3417cca34c7a4bb40e6e7a3cd307d853cbd6c86
Scott Jacobsen <jacobsenscott@yahoo.com>
2015-01-28 08:36:46 -0700

    Update settings

--- custom/25inf-ruby.el
+++ custom/25inf-ruby.el
@@ -1,8 +1,8 @@
-(setq initial-major-mode 'enh-ruby-mode)
-(setq initial-scratch-message "\
-# This buffer is for notes you don't want to save, and for Ruby code.
-# If you want to create a file, visit that file with C-x C-f,
-# then enter the text in that file's own buffer.")
+;; (setq initial-major-mode 'enh-ruby-mode)
+;; (setq initial-scratch-message "\
+;; # This buffer is for notes you don't want to save, and for Ruby code.
+;; # If you want to create a file, visit that file with C-x C-f,
+;; # then enter the text in that file's own buffer.")
 
 
 (add-hook 'inf-ruby-mode (lambda() (local-set-key (kbd "C-p") #'comint-previous-input)))
---
+++ custom/32org.el
@@ -0,0 +1,5 @@
+(setq org-clock-persist 'history)
+(org-clock-persistence-insinuate)
+(setq org-clock-idle-time 10)
+
+(provide '32org)
