2cf8a58912b562bead23de4d3eed4b1a5bfa91e3
Scott Jacobsen <jacobsenscott@yahoo.com>
2015-02-16 11:27:54 -0700

    Enable discover mode, add function to delete file and kill buffere

--- custom/000global.el
+++ custom/000global.el
@@ -69,4 +69,18 @@
      (clear-visited-file-modtime))
 (global-set-key (kbd "C-c C-t") 'touch)
 
+(global-discover-mode 1)
+
+(defun delete-file-and-buffer ()
+  "Kill the current buffer and deletes the file it is visiting."
+  (interactive)
+  (let ((filename (buffer-file-name)))
+    (when filename
+      (if (vc-backend filename)
+          (vc-delete-file filename)
+        (progn
+          (delete-file filename)
+          (message "Deleted file %s" filename)
+          (kill-buffer))))))
+
 (provide '000global)
