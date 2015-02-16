0732e014cc6add3a135637fddc7d1ef2b3c8e8f3
Scott Jacobsen <jacobsenscott@yahoo.com>
2015-02-16 11:28:37 -0700

    Better helm

--- custom/001helm.el
+++ custom/001helm.el
@@ -5,5 +5,8 @@
 (require 'helm-config)
 (global-set-key (kbd "M-x") 'helm-M-x)
 (setq helm-M-x-fuzzy-match t)
+(setq helm-buffers-fuzzy-matching t)
+(setq helm-buffer-max-length 50)
+(global-set-key (kbd "C-x b") 'helm-buffers-list)
 (provide '001helm)
 ;;; 001helm.el ends here
