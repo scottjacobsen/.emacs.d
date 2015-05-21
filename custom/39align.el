9a455e79f3d48ac21d54e2cd9c22763a3fceef1c
Scott Jacobsen <jacobsenscott@yahoo.com>
2015-05-20 21:16:02 -0600

    Make align work with ruby hashes

---
+++ custom/39align.el
@@ -0,0 +1,35 @@
+;;; Package -- align ruby hashes
+;;; Commentary:
+;;; Code:
+(require 'align)
+
+(defconst align-ruby-modes '(ruby-mode)
+  "Align-perl-modes is a variable defined in `align.el'.")
+
+(defconst ruby-align-rules-list
+  '((ruby-comma-delimiter
+     (regexp . ",\\(\\s-*\\)[^/ \t\n]")
+     (modes  . align-ruby-modes)
+     (repeat . t))
+    (ruby-string-after-func
+     (regexp . "^\\s-*[a-zA-Z0-9.:?_]+\\(\\s-+\\)['\"]\\w+['\"]")
+     (modes  . align-ruby-modes)
+     (repeat . t))
+    (ruby-symbol-after-func
+     (regexp . "^\\s-*[a-zA-Z0-9.:?_]+\\(\\s-+\\):\\w+")
+     (modes  . align-ruby-modes))
+    (ruby-new-style-hash
+     (regexp . "^\\s-*[a-zA-Z0-9.:?_]+:\\(\\s-+\\)[a-zA-Z0-9:'\"]") ;; This guy needs more work.
+     (modes  . align-ruby-modes)))
+  "Alignment rules specific to the ruby mode.
+See the variable `align-rules-list' for more details.")
+
+(add-to-list 'align-perl-modes 'ruby-mode)
+(add-to-list 'align-dq-string-modes 'ruby-mode)
+(add-to-list 'align-sq-string-modes 'ruby-mode)
+(add-to-list 'align-open-comment-modes 'ruby-mode)
+(dolist (it ruby-align-rules-list)
+  (add-to-list 'align-rules-list it))
+
+(provide '39align)
+;;; 39align.el ends here
